414584c4c5ef5e78b6be559f74b40bc35502d29d
aktnb <70847394+aktnb@users.noreply.github.com>
2025-03-02 19:32:25 +0900

    open session

--- main.go
+++ main.go
@@ -1,8 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var s *discordgo.Session
+
+var (
+	BotToken string
+)
+
+func init() {
+	BotToken = os.Getenv("DISCORD_BOT_TOKEN")
+}
 
 func main() {
-	// Your code here
-	fmt.Println("Hello, World!")
+	var err error
+	s, err = discordgo.New("Bot " + BotToken)
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
+	}
+
+	s.AddHandler(func(s *discordgo.Session, m *discordgo.Ready) {
+		log.Printf("Logged in as: %v#%v\n", m.User.Username, m.User.Discriminator)
+	})
+
+	err = s.Open()
+	if err != nil {
+		log.Fatalf("Error opening connection to Discord: %v", err)
+	}
+	defer s.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Bot is now running. Press CTRL+C to exit.")
+	<-stop
 }
